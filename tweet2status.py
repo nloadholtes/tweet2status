636e0ca41fb06d1f10760cc674a07e800d6c4300
Nick Loadholtes <nick@ironboundsoftware.com>
2012-03-13 21:36:32 -0400

    Starting the xmpp stuff

--- tweet2status.py
+++ tweet2status.py
@@ -7,14 +7,30 @@
 
 import urllib2
 import json
+import ConfigParser
+import codecs
+import xmpp
+
+def updateStatus(config, status):
+    print(status)
 
 def main(configfilename='config.cfg'):
     config = ConfigParser.RawConfigParser()
     config.readfp(codecs.open(configfilename, "r", "utf8"))
 
     url = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name='
-    req = urllib2.open(url)
-    
+    url += config.get('twitter', 'username')
+    req = urllib2.Request(url)
+    f = urllib2.urlopen(req)
+    response = f.read()
+    tweets = json.loads(response)
+    status = ''
+    for tweet in tweets:
+        if tweet['text'][0] == '@':
+            continue
+        status = tweet['text']
+        break
+    updateStatus(config, status)
 
 if __name__ == "__main__":
     main()
