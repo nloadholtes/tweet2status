e5150e039efb9f998dc4e7648f4b75fa40f988be
Nick Loadholtes <nick@ironboundsoftware.com>
2012-03-13 22:16:47 -0400

    Debuging the posting of the status

--- tweet2status.py
+++ tweet2status.py
@@ -10,9 +10,48 @@ import json
 import ConfigParser
 import codecs
 import xmpp
+import logging
 
-def updateStatus(config, status):
-    print(status)
+log = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+log.addHandler(ch)
+
+def updateStatus(config, newstatus):
+    print(newstatus)
+    jid = xmpp.protocol.JID(config.get('gtalk', 'gtalkname'))
+    cl = xmpp.Client(jid.getDomain(),debug=[])
+    if not cl.connect(('talk.google.com',5222)):
+        log.error('Can not connect to server.')
+        return
+    if not cl.auth(jid.getNode(), config.get('gtalk', 'gtalkpassword')):
+        log.error('Can not auth with server')
+        return
+
+    iq = xmpp.Iq()
+    iq.setType('get')
+    iq.setTo(config.get('gtalk', 'gtalkname'))
+    node = xmpp.Node()
+    node.setName('query')
+    node.setAttr('xmlns', 'google:shared-status')
+    iq.addChild(node=node)
+    cl.RegisterHandler('iq', handler)
+    cl.send(iq)
+    cl.Process(1)
+    cl.disconnect()
+
+
+def handler(conn, node):
+    node0 = node.getChildren()[0]
+    node0.delAttr('status-list-max')
+    node0.delAttr('status-max')
+    node0.delAttr('status-list-contents-max')
+    status = node0.getChildren()[0]
+    if status.getData() == "BLAH":
+        return
+    status.setData("BLAH")
+    node.setType('set')
+    conn.send(node)
 
 def main(configfilename='config.cfg'):
     config = ConfigParser.RawConfigParser()
